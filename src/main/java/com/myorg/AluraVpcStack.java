a06f5ae14d5ed70de10bff10625f1627c32dc7d5
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-04-16 08:59:53 -0300

    commit inicial

--- src/main/java/com/myorg/AluraVpcStack.java
+++ src/main/java/com/myorg/AluraVpcStack.java
@@ -1,16 +1,16 @@
 package com.myorg;
 
-import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.constructs.Construct;
 
 
-public class AluraAwsInfraStack extends Stack {
-    public AluraAwsInfraStack(final Construct scope, final String id) {
+public class AluraVpcStack extends Stack {
+    public AluraVpcStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
 
-    public AluraAwsInfraStack(final Construct scope, final String id, final StackProps props) {
+    public AluraVpcStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
         // The code that defines your stack goes here
